e2c3bda39879812ea2224514ef53a340efe76640
Semir Pljakić <93542041+semirpljakic@users.noreply.github.com>
2023-01-26 11:44:34 +0100

    Update Modal.js

--- src/Modal.js
+++ src/Modal.js
@@ -11,7 +11,7 @@ const Modal = () => {
     >
       <div className='modal-container'>
         <h3>modal content</h3>
-        <button className='close-modal-btn' onMouseDown={closeModal}>
+        <button className='close-modal-btn' onClick={closeModal}>
           <FaTimes></FaTimes>
         </button>
       </div>
